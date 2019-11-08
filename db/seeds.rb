e0ebf32b84245947cf1d957a23b5bb568dac262a
Thomas <thomas@craftacademy.se>
2019-11-08 16:40:08 +0100

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+Skill.destroy_all
+Category.destroy_all
+
+[
+  {
+    name: 'FRONT END',
+    technologies: [
+      { name: 'html' },
+      { name: 'html5' },
+      { name: 'java script' },
+      { name: 'J query' },
+      { name: 'CSS3' }
+    ]
+  },
+  {
+    name: 'BACK END',
+    technologies: [
+      { name: 'Ruby on Rails' },
+      { name: 'PHP' },
+      { name: 'Angular2' },
+      { name: 'Node.js' },
+      { name: '.Net' }
+    ]
+  },
+  {
+    name: 'DATABASE',
+    technologies: [
+      { name: 'mySQL' },
+      { name: 'MongoDB' },
+      { name: 'CouchDB' },
+      { name: 'MS SQL' }
+    ]
+  },
+  {
+    name: 'VERSION CONTROL',
+    technologies: [
+      { name: 'GIT' },
+      { name: 'grunt' },
+      { name: 'Xdebug' },
+      { name: 'Subversion' }
+    ]
+  },
+  {
+    name: 'PROJECT MGMT TOOLS',
+    technologies: [
+      { name: 'Basecamp' },
+      { name: 'Jira' },
+      { name: 'Trello' },
+      { name: 'Redmin' },
+      { name: 'Teamwork' }
+    ]
+  }
+].each do |category|
+  new_category = Category.create(title: category[:name])
+  category[:technologies].each do |skill|
+    Skill.create(category: new_category, title: skill[:name])
+  end
+end
